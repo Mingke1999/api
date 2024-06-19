524b5a447cc06fadf8c499b6adcee23fe301769d
mingke-utas <95258603+Mingke1999@users.noreply.github.com>
2024-06-19 23:15:42 +1000

    refactor

--- Repository/StockRepo.cs
+++ Repository/StockRepo.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.DTOs.Stock;
 using api.interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -13,9 +14,50 @@ namespace api.Repository
         {
             _context = context;
         }
-        public Task<List<Stock>> GetAllAsync()
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
         {
-            return _context.Stocks.ToListAsync();
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=> x.stockId == id);
+            if(stockModel == null){
+                return null;
+            }
+            _context.Stocks.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.FindAsync(id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateDto)
+        {
+            var existingStock = await _context.Stocks.FirstOrDefaultAsync(x => x.stockId == id);
+            if(existingStock == null){
+                return null;
+            }
+                existingStock.Symbol = updateDto.Symbol;
+                existingStock.CompanyName = updateDto.CompanyName;
+                existingStock.Purchase = updateDto.Purchase;
+                existingStock.Industry = updateDto.Industry;
+                existingStock.Divdend = updateDto.Divdend;
+                existingStock.MarketCap = updateDto.MarketCap;
+
+                await _context.SaveChangesAsync();
+                return existingStock;
         }
     }
 }
--- interfaces/IStockRepository.cs
+++ interfaces/IStockRepository.cs
@@ -1,3 +1,4 @@
+using api.DTOs.Stock;
 using api.Models;
 
 namespace api.interfaces
@@ -5,5 +6,9 @@ namespace api.interfaces
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateDto);
+        Task<Stock?> DeleteAsync(int id);
     }
 }

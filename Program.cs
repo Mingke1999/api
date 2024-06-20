2d2ae169096a3c826e848c2b5721f69396bdc3e6
mingke-utas <95258603+Mingke1999@users.noreply.github.com>
2024-06-21 00:36:41 +1000

    authentications

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using api.Data;
 using api.interfaces;
+using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +23,37 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>{
 });
 builder.Services.AddScoped<IStockRepository, StockRepo>();
 builder.Services.AddScoped<ICommentRepository, CommentRepo>();
+builder.Services.AddIdentity<User, IdentityRole>(options =>{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+})
+.AddEntityFrameworkStores<ApplicationDBContext>();
+builder.Services.AddAuthentication(options =>{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme = 
+    options.DefaultForbidScheme = 
+    options.DefaultScheme =
+    options.DefaultSignInScheme = 
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+    
+}).AddJwtBearer(
+    options =>{
+        options.TokenValidationParameters = new TokenValidationParameters{
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+            )
 
+        };
+    }
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +65,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.MapControllers(); //add controller
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
